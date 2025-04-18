2c0d65f74831329dc41ca873b578bca7d518bf60
SergioOrellanaR <orellana123@gmail.com>
2025-04-18 14:40:27 -0400

    Live search integration
    Double up integration
    
    Live search
    
    Update player names.
    
    Get players call
    
    Search player implementation step 1
    
    Search button

---
+++ config.js
@@ -0,0 +1,43 @@
+export const CONFIG = {
+    colors: ['#ff4c4c', '#4c6aff', '#4cff9a', '#ffffff', '#c74cff', '#ffb703', '#4cffe9'],
+    coreItems: [
+        "Guinsoo's Rageblade", "Archangel's Staff", "Blue Buff", "Spear of Shojin",
+        "Jeweled Gauntlet", "Infinity Edge", "Nashor's Tooth", "Morellonomicon",
+        "Runaan's Hurricane", "Hextech Gunblade", "Bloodthirster", "Edge of Night",
+        "Titan's Resolve", "Hand Of Justice"
+    ],
+    tierColors: {
+        S: '#FFD700',
+        A: '#00BFFF',
+        B: '#7CFC00',
+        C: '#FFB347'
+    },
+    routes: {
+        comps: 'Data/Comps.csv',
+        items: 'Data/Items.csv',
+        units: 'Data/Units.csv'
+    },
+    iconOptions: [
+        { name: 'Moon', color: '#c74cff', emoji: '🌙' },
+        { name: 'Fire', color: '#ff69b4', emoji: '🔥' },
+        { name: 'Water', color: '#4cffe9', emoji: '💧' },
+        { name: 'Thunder', color: '#ffee4c', emoji: '⚡' }
+    ],
+    serverRegionMap : {
+        NA: "NA1",
+        BR: "BR1",
+        EUNE: "EUN1",
+        EUW: "EUW1",
+        JP: "JP1",
+        KR: "KR",
+        LAN: "LA1",
+        LAS: "LA2",
+        ME: "ME1",
+        OCE: "OC1",
+        RU: "RU",
+        SEA: "SG2",
+        TR: "TR1",
+        TW: "TW2",
+        VN: "VN2"
+    }
+};
