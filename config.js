60dfde25ffbc181382e8e36280ef643ec2c1df39
SergioOrellanaR <orellana123@gmail.com>
2025-04-23 01:30:05 -0400

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ export const CDRAGON_URL = {
 }
 
 export const CONFIG = {
-    netlify: false,
+    netlify: true,
     colors: ['#ff4c4c', '#4c6aff', '#4cff9a', '#ffffff', '#c74cff', '#ffb703', '#4cffe9'],
     coreItems: [
         "Guinsoo's Rageblade", "Archangel's Staff", "Blue Buff", "Spear of Shojin",
